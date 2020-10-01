e590b6f5abf4a49811a4de47612177968032917b
PABlond <pierre-alexis.blond@live.fr>
2020-10-01 15:46:28 +0200

    update src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,13 @@ import React from "react"
 import styled, { createGlobalStyle } from "styled-components"
 
 import { Column } from "../components"
-import { BackLayout, MobileContainer } from "../containers"
+import { BackLayout, MobileContainer, Texts } from "../containers"
 import { colors } from "../utils"
 
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
+    font-family: 'Rubik', sans-serif;
   }
 `
 
@@ -18,96 +19,6 @@ const Container = styled.div`
   align-items: flex-start;
   min-height: 100vh;
   background: ${colors.secondary.lightGrayViolet};
-
-  .mobile-container {
-    .mobile-sub-container {
-      .header {
-        display: flex;
-        justify-content: center;
-        align-items: flex-end;
-        height: 4.125rem;
-        width: 100%;
-        background: red;
-        border-radius: 2rem 2rem 0.5rem 0.5rem;
-        position: relative;
-        background: linear-gradient(
-          to left,
-          ${colors.gradients.magenta},
-          ${colors.gradients.violet}
-        );
-
-        .header-top {
-          position: absolute;
-          background: white;
-          top: 0;
-          width: 8.1875rem;
-          height: 1rem;
-          border-radius: 0 0 0.8rem 0.8rem;
-        }
-
-        .interlocutor {
-          height: 48px;
-          width: 100%;
-          color: white;
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-
-          .interlocutor-left {
-            width: 12.875rem;
-            display: flex;
-            justify-content: flex-start;
-            align-items: center;
-
-            span {
-              display: flex;
-              align-items: center;
-              margin-left: 0.5rem;
-
-              svg {
-                font-size: 0.7rem;
-                stroke-width: 1.5;
-                transform: scaleY(1.7);
-              }
-            }
-
-            div {
-              h3,
-              h4 {
-                margin: 0;
-              }
-
-              h3 {
-                font-size: 13px;
-              }
-
-              h4 {
-                font-size: 6px;
-                color: #d879ff;
-              }
-            }
-
-            .avatar-container {
-              width: 2.5rem;
-
-              .avatar {
-                margin: auto;
-                width: 24px;
-                height: 24px;
-                border-radius: 50%;
-                border: 1px solid white;
-              }
-            }
-          }
-
-          .interlocutor-right {
-            transform: rotate(90deg);
-            font-weight: bold;
-          }
-        }
-      }
-    }
-  }
 `
 
 const Wrapper = styled.div`
@@ -133,15 +44,7 @@ export default () => {
             <Column.Mobile className="mobile-container">
               <MobileContainer />
             </Column.Mobile>
-            <Column.Description className="description-container">
-              <h1>Simple booking</h1>
-              <p>
-                Stay in touch with our dog walkers through the chat interface.
-                This makes it easy to discuss arrangements and make bookings.
-                Once the walk has been completed you can rate your walker and
-                book again all through the chat.
-              </p>
-            </Column.Description>
+            <Texts />
           </BackLayout>
         </Wrapper>
       </Container>
