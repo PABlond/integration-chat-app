1a70e7e469c0fd966ed3fa37c683d74aaf8ff4ed
PABlond <pierre-alexis.blond@live.fr>
2020-10-01 14:58:12 +0200

    update src/utils/colors.tsx

---
+++ src/utils/colors.tsx
@@ -0,0 +1,20 @@
+export const colors = {
+  text: {
+    paleViolet: "hsl(276, 100%, 81%)",
+    moderateViolet: "hsl(276, 55%, 52%)",
+    darkViolet: "hsl(271, 15%, 43%)",
+    veryDarkViolet: "hsl(271, 36%, 24%)",
+    grayBlue: "hsl(206, 6%, 79%)",
+    darkGrayViolet: "hsl(270, 7%, 64%)",
+  },
+  gradients: {
+    magenta: "hsl(293, 100%, 63%)",
+    violet: "hsl(264, 100%, 61%)",
+  },
+  secondary: {
+    white: "hsl(0, 0%, 100%)",
+    lightGrayViolet: "hsl(270, 20%, 96%)",
+    veryDarkViolet: "hsl(271, 36%, 24%)",
+    lightMagenta: "hsl(289, 100%, 72%)",
+  },
+}
