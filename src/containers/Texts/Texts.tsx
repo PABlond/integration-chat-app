6755ea1ffa2140a8b39c0668fbbb3af9b11c63e4
PABlond <pierre-alexis.blond@live.fr>
2020-10-01 15:46:28 +0200

    update src/containers/Texts/Texts.tsx

---
+++ src/containers/Texts/Texts.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+import { Column } from "../../components"
+
+export const Texts = () => {
+  return (
+    <Column.Description className="description-container">
+      <h1>Simple booking</h1>
+      <p>
+        Stay in touch with our dog walkers through the chat interface. This
+        makes it easy to discuss arrangements and make bookings. Once the walk
+        has been completed you can rate your walker and book again all through
+        the chat.
+      </p>
+    </Column.Description>
+  )
+}
