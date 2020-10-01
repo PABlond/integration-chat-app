9d6d94460c5c58443ad532df3f3b6c91f3744c66
PABlond <pierre-alexis.blond@live.fr>
2020-10-01 14:58:12 +0200

    update src/containers/Mobile/MobileView/MobileView.tsx

---
+++ src/containers/Mobile/MobileView/MobileView.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+import styled from "styled-components"
+
+const Container = styled.div`
+  width: 15.625rem;
+  height: 31.5rem;
+  border-radius: 2rem;
+  padding: 0.6875rem;
+  z-index: 2;
+  background: white;
+  position: relative;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+`
+
+export const MobileView = ({ children, ...rest }) => {
+  return <Container {...rest}>{children}</Container>
+}
